18c0776a4d662579f4f7ffb14902e613fc06cddf
zburzhynski <zburzhynski@gmail.com>
2021-10-05 16:40:52 +0300

    Refined extra methods generator.

--- src/main/java/ExtraMethodGenerator.java
+++ src/main/java/ExtraMethodGenerator.java
@@ -4,12 +4,16 @@ import org.apache.commons.lang3.StringUtils;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Extra methods mapstruct generator.
  */
 public class ExtraMethodGenerator {
 
+    private static final String LIST_IMPORT = "import java.util.List;";
+    private static final String IMPORT_TEMPLATE = "import %s;";
     private static final String MAPPER_NAME = "ExtraMethodMapper";
     private static final String MAPPER_INTERFACE = "public interface ExtraMethodMapper" + CommonConstant.SPACE + CommonConstant.LEFT_BRACKET;
     private static final String METHOD_TEMPLATE = "List<%s.%s> to%sList(List<%s> %sList);";
@@ -25,19 +29,31 @@ public class ExtraMethodGenerator {
 
     public void generate() throws IOException {
         writer = new FileWriter(MAPPER_NAME + CommonConstant.DOT + CommonConstant.JAVA_EXTENSION);
+        Set<String> sourceClasses = dozerConfig.getMappings().stream().map(Mapping::getClassA).collect(Collectors.toSet());
+        generateImports(sourceClasses);
+        writer.write(CommonConstant.LINE_BREAK);
         writer.write(MAPPER_INTERFACE);
         writer.write(CommonConstant.LINE_BREAK);
         writer.write(CommonConstant.LINE_BREAK);
-        for (Mapping mapping : dozerConfig.getMappings()) {
-            generateMethod(mapping);
+        for (String source : sourceClasses) {
+            generateMethod(source);
         }
         writer.write(CommonConstant.RIGHT_BRACKET);
         writer.close();
     }
 
-    private void generateMethod(Mapping mapping) throws IOException {
-        String sourceClass = mapping.getClassA();
-        String sourceEntityName = sourceClass.substring(sourceClass.lastIndexOf(CommonConstant.DOT) + 1);
+    private void generateImports(Set<String> sourceClasses) throws IOException {
+        writer.write(LIST_IMPORT);
+        writer.write(CommonConstant.LINE_BREAK);
+        for (String source : sourceClasses) {
+            String importName = String.format(IMPORT_TEMPLATE, source);
+            writer.write(importName);
+            writer.write(CommonConstant.LINE_BREAK);
+        }
+    }
+
+    private void generateMethod(String source) throws IOException {
+        String sourceEntityName = source.substring(source.lastIndexOf(CommonConstant.DOT) + 1);
         String method = String.format(METHOD_TEMPLATE, dtoPackage, sourceEntityName, sourceEntityName, sourceEntityName,
             StringUtils.uncapitalize(sourceEntityName));
         writer.append(CommonConstant.TAB_SPACE);
